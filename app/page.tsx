fc9f453224b0688470f27a00abae22c94a2a5684
Authomasation <authomasation@gmail.com>
2025-09-05 21:53:38 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,137 @@
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+const baseButton = "h-12 rounded-full px-5 py-2 font-medium text-sm sm:text-base transition-colors flex items-center gap-2";
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+export default function Home() {
+    return (
+        <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+            <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        
+                <h1 className="text-4xl sm:text-5xl font-extrabold text-center text-gray-900 dark:text-gray-100 tracking-wide">
+  BudgetBelastingen
+</h1>
+
+
+                <div className="flex gap-4 flex-col sm:flex-row">
+                    <a className={`${baseButton} bg-blue-600 text-white hover:bg-green-600`}
+                        href="#"
+                        //target="_blank"
+                        rel="noopener noreferrer">
+                        <Image
+                            className="dark:invert"
+                            src="/add.svg"
+                            alt="toevoegen"
+                            width={20}
+                            height={20}
+                        />
+                        Nieuw Bonnetje
+                    </a>
+                    <a className={`${baseButton} border border-gray-300 hover:bg-gray-100 hover:text-[#333]`}
+                        href="#"
+                        //target="_blank"
+                        rel="noopener noreferrer">
+                        <Image
+                            className="dark:invert"
+                            src="/edit.svg"
+                            alt="aanpassen"
+                            width={20}
+                            height={20}
+                        />
+                        Bekijk/Wijzig Bonnetjes
+                    </a>
+                </div>
+                <div className="flex gap-4 flex-col sm:flex-row">
+                    <a className={`${baseButton} border border-gray-300 hover:bg-gray-100 hover:text-[#333]`}
+                        href="#"
+                        //target="_blank"
+                        rel="noopener noreferrer">
+                        <Image
+                            className="dark:invert"
+                            src="/dashboards.svg"
+                            alt="dashboard"
+                            width={20}
+                            height={20}
+                        />
+                        Dashboard
+                    </a>
+                    <a className={`${baseButton} border border-gray-300 hover:bg-gray-100 hover:text-[#333]`}
+                        href="#"
+                        //target="_blank"
+                        rel="noopener noreferrer">
+                        <Image
+                            className="dark:invert"
+                            src="/excel.svg"
+                            alt="excel download"
+                            width={20}
+                            height={20}
+                        />
+                        Download bonnetjes (Excel)
+                    </a>
+                </div>
+               
+            </main>
+            <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+                <a
+                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+                    href="#"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <Image
+                        aria-hidden
+                        src="/login.svg"
+                        alt="login icon"
+                        width={16}
+                        height={16}
+                    />
+                    Login
+                </a>
+                <a
+                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+                    href="#"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <Image
+                        aria-hidden
+                        src="/contact.svg"
+                        alt="contact icon"
+                        width={16}
+                        height={16}
+                    />
+                    Contact
+                </a>
+                <a
+                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+                    href="#"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <Image
+                        aria-hidden
+                        src="/disclaimer.svg"
+                        alt="disclaimer icon"
+                        width={16}
+                        height={16}
+                    />
+                    Disclaimer
+                </a>
+                <a
+                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+                    href="https://www.belastingdienst.nl/wps/wcm/connect/bldcontentnl/belastingdienst/zakelijk/btw/btw_aangifte_doen_en_betalen/btw-aangifte-waar-moet-u-aan-denken/hoe-btw-aangifte-invullen-en-versturen"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <Image
+                        aria-hidden
+                        src="/bld_logo.svg"
+                        alt="Belastingdienst logo"
+                        width={16}
+                        height={16}
+                    />
+                    Doe hier uw aangifte →
+                </a>
+            </footer>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
